9bde06842c627b6e64d3ae12fadf2c31021e8720
Hilton Fernandes @ Titanium <hilton.fernandes@titaniumasset.com.br>
2022-01-17 17:06:59 -0300

    2022-01-17 Mid work: created 1st version, just as the author Catalin's Tech proposed

---
+++ index.js
@@ -0,0 +1,13 @@
+/**  From https://betterprogramming.pub/build-a-simple-web-server-using-node-js-and-express-27f3d6eb4e86 */
+
+const path = require('path');
+const express = require('express');
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+
+    res.sendFile(`${__dirname}/public/index.html`);
+});app.listen(3333, () => {
+    console.log('Application listening on port 3333!');
+});
