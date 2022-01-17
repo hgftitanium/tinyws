319c54f605b24b11c1f4e565cf6520cb1a590afd
Hilton Fernandes @ Titanium <hilton.fernandes@titaniumasset.com.br>
2022-01-17 18:17:37 -0300

    2022-01-17 Mid work: Adding minimal.html, 1st attempt of a Web interface for 'Bot SF'

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-
+    
     res.sendFile(`${__dirname}/public/index.html`);
-});app.listen(3333, () => {
+});
+
+app.get('/minimal', (req, res) => {
+    
+    res.sendFile(`${__dirname}/public/minimal.html`);
+});
+
+app.listen(3333, () => {
     console.log('Application listening on port 3333!');
 });
