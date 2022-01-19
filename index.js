5409631f74c84a5269864d9c9e3779a2244ecf29
Hilton Fernandes @ Titanium <hilton.fernandes@titaniumasset.com.br>
2022-01-19 16:34:50 -0300

    2022-01-19 Mid work: After studying a little JavaScript and Python flask, trying to express BotSF into it

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-/**  From https://betterprogramming.pub/build-a-simple-web-server-using-node-js-and-express-27f3d6eb4e86 */
+/**  From 
+ * https://betterprogramming.pub/build-a-simple-web-server-using-node-js-and-express-27f3d6eb4e86 
+ * https://stackoverflow.com/questions/23691194/node-express-file-upload
+*/
 
 const path = require('path');
 const express = require('express');
-const app = express()
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+// default options
+app.use(fileUpload());
+/* 
+var listAttributes = function (obj) {
+    var keys = [];
+    for(var key in obj){
+       keys.push(key);
+    }
+    return keys;
+}
+
+*/
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
@@ -13,6 +31,35 @@ app.get('/', (req, res) => {
 app.get('/minimal', (req, res) => {
     
     res.sendFile(`${__dirname}/public/minimal.html`);
+});
+
+app.post('/fileupload', (req, res) => {
+    console.log('req' + req)
+//    console.log(listAttributes(req))
+
+    console.log('res' + res)
+//    console.log(listAttributes(res))
+
+//    res.sendFile(`${__dirname}/public/fileupload.html`);
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(`${__dirname}/some.ini`, function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
+
+app.get('/fileupload', (req, res) => {
+    
+    res.sendFile(`${__dirname}/public/fileupload.html`);
 });
 
 app.listen(3333, () => {
